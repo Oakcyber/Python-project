5d5718ec1c37b1ad0819996d627dbc73ec73055e
Oakcyber <104633306+Oakcyber@users.noreply.github.com>
2022-10-19 04:49:07 +0100

    Add files via upload

---
+++ Condition.py
@@ -0,0 +1,11 @@
+Name = (input("What is your name?"))
+if Name == "Augustine":
+    print("Correct name")
+else:
+    print("Get out!")
+
+Age = (input("What is your age?"))
+if Age == "30":
+    print("Allowed")
+else:
+    print("You are not allowed")
